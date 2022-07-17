db85e1899d9f592600ab760f946c0806046bff81
wangxiaosong <wangxiaosong1234@hotmail.com>
2022-07-17 12:43:33 +0800

    -feat: dev server

--- webpack.config.js
+++ webpack.config.js
@@ -120,7 +120,7 @@ module.exports = function () {
         // 配置代理（只在本地开发有效，上线无效）
         "/api": {
           target: "http://localhost:3000", // 这是本地用node写的一个服务，用webpack-dev-server起的服务默认端口是8080
-          pathRewrite: { "/api": "" }, // 后台在转接的时候url中是没有 /api 的
+          // pathRewrite: { "/api": "" }, // 后台在转接的时候url中是没有 /api 的
           changeOrigin: true, // 加了这个属性，那后端收到的请求头中的host是目标地址 target
         },
       },
